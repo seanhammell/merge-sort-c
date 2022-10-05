8b8830d7c9fe8a641e3294ecc85d928838cd33ad
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-05 16:40:36 -0500

    Add merge sort for characters

--- merge-sort.c
+++ merge-sort.c
@@ -7,7 +7,7 @@
 #define KRED "\x1B[31m"
 #define KGRN "\x1B[32m"
 
-void msort(double* list, int size) {
+void msort_doubles(double* list, int size) {
     if (size == 1) {
         return;
     }
@@ -19,13 +19,60 @@ void msort(double* list, int size) {
     for (int i = 0; i < lsize; ++i) {
         left[i] = list[i];
     }
-    msort(left, lsize);
+    msort_doubles(left, lsize);
 
     double right[rsize];
     for (int i = 0; i < rsize; ++i) {
         right[i] = list[lsize+i];
     }
-    msort(right, rsize);
+    msort_doubles(right, rsize);
+
+    int i = 0;
+    int j = 0;
+    while (i + j < size) {
+        if (i == lsize) {
+            while (j < rsize) {
+                list[i+j] = right[j];
+                ++j;
+            }
+        } else if (j == rsize) {
+            while (i < lsize) {
+                list[i+j] = left[i];
+                ++i;
+            }
+        } else {
+            if (left[i] < right[j]) {
+                list[i+j] = left[i];
+                ++i;
+            } else {
+                list[i+j] = right[j];
+                ++j;
+            }
+        }
+    }
+
+    return;
+}
+
+void msort_chars(char* list, int size) {
+    if (size == 1) {
+        return;
+    }
+
+    int lsize = size / 2;
+    int rsize = size - lsize;
+
+    char left[lsize];
+    for (int i = 0; i < lsize; ++i) {
+        left[i] = list[i];
+    }
+    msort_chars(left, lsize);
+
+    char right[rsize];
+    for (int i = 0; i < rsize; ++i) {
+        right[i] = list[lsize+i];
+    }
+    msort_chars(right, rsize);
 
     int i = 0;
     int j = 0;
@@ -57,7 +104,7 @@ void msort(double* list, int size) {
 void test_merge_sort_doubles() {
     double list[5] = {1, 5, 2, 4, 3};
     double checklist[5] = {1, 2, 3, 4, 5};
-    msort(list, 5);
+    msort_doubles(list, 5);
 
     int i = 0;
     while (i < 5) {
@@ -75,6 +122,27 @@ void test_merge_sort_doubles() {
     printf("%stest merge sort doubles\n", KNRM);
 }
 
+void test_merge_sort_chars() {
+    char list[5] = {'a', 'e', 'b', 'd', 'c'};
+    char checklist[5] = {'a', 'b', 'c', 'd', 'e'};
+    msort_chars(list, 5);
+
+    int i = 0;
+    while (i < 5) {
+        if (list[i] != checklist[i]) {
+            break;
+        }
+        ++i;
+    }
+
+    if (i == 5) {
+        printf("%s[PASSED] ", KGRN);
+    } else {
+        printf("%s[FAILED] ", KRED);
+    }
+    printf("%stest merge sort chars\n", KNRM);
+}
+
 int main(int argc, char* argv[argc+1]) {
     if (argc > 1) {
         double list[argc-1];
@@ -84,7 +152,7 @@ int main(int argc, char* argv[argc+1]) {
         }
         printf("\n");
 
-        msort(list, argc-1);
+        msort_doubles(list, argc-1);
 
         for (size_t i = 0; i < argc-1; ++i) {
             printf("%.2f ", list[i]);
@@ -92,6 +160,7 @@ int main(int argc, char* argv[argc+1]) {
         printf("\n");
     } else {
         test_merge_sort_doubles();
+        test_merge_sort_chars();
     }
 
     return EXIT_SUCCESS;
