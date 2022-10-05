f8a5331ad455ba6cfe80cae6cc201af0eec52080
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-05 16:27:51 -0500

    Add test merge sort doubles

--- merge-sort.c
+++ merge-sort.c
@@ -1,6 +1,11 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define KNRM "\x1b[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
 
 void msort(double* list, int size) {
     if (size == 1) {
@@ -49,20 +54,45 @@ void msort(double* list, int size) {
     return;
 }
 
+void test_merge_sort_doubles() {
+    double list[5] = {1, 5, 2, 4, 3};
+    double checklist[5] = {1, 2, 3, 4, 5};
+    msort(list, 5);
+
+    int i = 0;
+    while (i < 5) {
+        if (list[i] != checklist[i]) {
+            break;
+        }
+        ++i;
+    }
+
+    if (i == 5) {
+        printf("%s[PASSED] ", KGRN);
+    } else {
+        printf("%s[FAILED] ", KRED);
+    }
+    printf("%stest merge sort doubles\n", KNRM);
+}
+
 int main(int argc, char* argv[argc+1]) {
-    double list[argc-1];
-    for (size_t i = 0; argv[i+1]; ++i) {
-        list[i] = strtod(argv[i+1], 0);
-        printf("%.2f ", list[i]);
-    }
-    printf("\n");
+    if (argc > 1) {
+        double list[argc-1];
+        for (size_t i = 0; argv[i+1]; ++i) {
+            list[i] = strtod(argv[i+1], 0);
+            printf("%.2f ", list[i]);
+        }
+        printf("\n");
 
-    msort(list, argc-1);
+        msort(list, argc-1);
 
-    for (size_t i = 0; i < argc-1; ++i) {
-        printf("%.2f ", list[i]);
+        for (size_t i = 0; i < argc-1; ++i) {
+            printf("%.2f ", list[i]);
+        }
+        printf("\n");
+    } else {
+        test_merge_sort_doubles();
     }
-    printf("\n");
 
     return EXIT_SUCCESS;
 }
